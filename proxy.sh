9de0bbbd6c782e05219199fd72ebff81d1ffcc6b
GOTOHELL CHECKER <144633408+gotohellme@users.noreply.github.com>
2023-09-17 11:42:39 +0700

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Update package list and install Squid
-sudo apt update
-sudo apt install -y squid
+wget https://raw.githubusercontent.com/serverok/squid-proxy-installer/master/squid3-install.sh -O squid3-install.sh
+sudo bash squid3-install.sh
 
+mkdir /etc/squid
 # Add a user for the proxy (replace 'h3h3' and 'h3h3' with your desired username and password)
 sudo htpasswd -bc /etc/squid/passwords h3h3 h3h3
 
-# Backup the original Squid configuration file
+# # Backup the original Squid configuration file
 sudo cp /etc/squid/squid.conf /etc/squid/squid.conf.bak
 
 # Create a new Squid configuration file with your settings
@@ -19,18 +19,13 @@ http_access allow authenticated
 http_port 3128
 EOL
 
-# Restart Squid to apply the changes
-sudo systemctl restart squid
-
 # Enable Squid to start on boot
-sudo systemctl enable squid
+sudo systemctl restart squid
+sudo systemctl reload squid
 
 # Allow incoming traffic on port 587 (TCP and UDP)
-sudo ufw allow 587/tcp
-sudo ufw allow 587/udp
-
-# Enable the firewall
-sudo ufw enable
+sudo ufw allow 587/tcp -y
+sudo ufw allow 587/udp -y
 
 # Print proxy information
 echo "Proxy is http://h3h3:h3h3@$(hostname -I | awk '{print $1}'):3128"
