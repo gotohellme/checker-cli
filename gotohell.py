4414fbdaba0fd4af20c5d3da6016bbfb01693ce6
GOTOHELL CHECKER <144633408+gotohellme@users.noreply.github.com>
2023-11-03 10:46:59 +0700

    Update gotohell.py

--- gotohell.py
+++ gotohell.py
@@ -13,12 +13,33 @@ fm = Fore.MAGENTA
 lg = Fore.LIGHTGREEN_EX
 lr = Fore.LIGHTRED_EX
 
-# Input API key, input file, and API option
 try:
+    text = """\
+    STRIPE API COST
+    LIVE - ¢2
+    DEAD - ¢1
+
+    MPARTNER API COST
+    LIVE - ¢3
+    DEAD - ¢2
+    INVALID CAPTCHA - ¢0
+    """
+    box_width = 35
+    banner = f"+{'-' * (box_width - 2)}+\n"
+    banner += f"|{fb}{'STRIPE API COST'.center(box_width - 2)}{rs}|\n"
+    banner += f"|{fg}{'LIVE - ¢2'.center(box_width - 2)}{rs}|\n"
+    banner += f"|{fr}{'DEAD - ¢1'.center(box_width - 2)}{rs}|\n"
+    banner += f"+{'-' * (box_width - 2)}+\n"
+    banner += f"|{fb}{'MPARTNER API COST'.center(box_width - 2)}{rs}|\n"
+    banner += f"|{fg}{'LIVE - ¢3'.center(box_width - 2)}{rs}|\n"
+    banner += f"|{fr}{'DEAD - ¢2'.center(box_width - 2)}{rs}|\n"
+    banner += f"|{fy}{'INVALID CAPTCHA - ¢0'.center(box_width - 2)}{rs}|\n"
+    banner += f"+{'-' * (box_width - 2)}+"
+    print(banner)
     # Input API key, input file, and API option
     api_key = input(f"INPUT API KEY : ")
     file_path = input(f"INPUT LIST FILE : ")
-    api_option = input(f"{fg}[1]{rs} STRIPE $0.70-$1.00 {fg}[2]{rs} STRIPE $5 {fg}[3]{rs} STRIPE PRE-AUTH : ")
+    api_option = input(f"{fg}[1]{rs} STRIPE $0.70-$1.00 {fg}\n[2]{rs} STRIPE $5 {fg}\n[3]{rs} MPARTNER $0.50-$2.00\nChoose your api : ")
 
     # Read the input file and store the values in a list
     with open(file_path, "r") as file:
@@ -30,34 +51,34 @@ try:
         
         # Request to the selected API
         response = requests.get(api_url)
-        
+        json_response = response.json()
+
         # Check for error response
-        if response.json() and "error" in response.json()[0] and response.json()[0]["error"] != "Check your list format!":
-            print(f"{fr}Error: {response.json()[0]['error']}{rs}")
-            break
-
-        # Extract values from the JSON response
-        json_data = response.json()[0]
-        status = json_data["status"]
-        api = json_data["api"]
-        balance = json_data["balance"]
-        cc = json_data["cc"]
-        bin_value = json_data["bin"]
-        message = json_data["message"]
-
-        # Determine the status and print accordingly
-        if status == 'LIVE':
-            print(f"| {fg}{status}{rs} | [{fc}{balance}{rs}] {fy}{cc}{rs} - {fm}{bin_value}{rs} - {lg}{message}{rs}")
-            with open("CC_LIVE.txt", "a") as file:
-                file.write(f"{cc} [{api}] {bin_value} - {message}\n")
-        elif status == 'DEAD':
-            print(f"| {fr}{status}{rs} | [{fc}{balance}{rs}] {fy}{cc}{rs} - {fm}{bin_value}{rs} - {lr}{message}{rs}")
-            with open("CC_DEAD.txt", "a") as file:
-                file.write(f"{cc} [{api}] {bin_value} - {message}\n")
+        if "error" in response.json()[0]:
+            print(f"| {fr}UNKNOWN{rs} | {fy}{value}{rs} - {lr}Error: {response.json()[0]['error']}{rs}")
         else:
-            print(f"| {fr}{status}{rs} | [{fc}{balance}{rs}] {fy}{cc}{rs} - {fm}{bin_value}{rs} - {lr}{message}{rs}")
-            with open("CC_UNKNOWN.txt", "a") as file:
-                file.write(f"{cc} [{api}] {bin_value} - {message}\n")
+            # Extract values from the JSON response and process them
+            json_data = response.json()[0]
+            status = json_data["status"]
+            api = json_data["api"]
+            balance = json_data["balance"]
+            cc = json_data["cc"]
+            bin_value = json_data["bin"]
+            message = json_data["message"]
+
+            # Determine the status and print accordingly
+            if status == 'LIVE':
+                print(f"| {fg}{status}{rs} | [{fc}{balance}{rs}] {fy}{cc}{rs} - {fm}{bin_value}{rs} - {lg}{message}{rs}")
+                with open("CC_LIVE.txt", "a") as file:
+                    file.write(f"{cc} [{api}] {bin_value} - {message}\n")
+            elif status == 'DEAD':
+                print(f"| {fr}{status}{rs} | [{fc}{balance}{rs}] {fy}{cc}{rs} - {fm}{bin_value}{rs} - {lr}{message}{rs}")
+                with open("CC_DEAD.txt", "a") as file:
+                    file.write(f"{cc} [{api}] {bin_value} - {message}\n")
+            else:
+                print(f"| {fy}{status}{rs} | [{fc}{balance}{rs}] {fy}{cc}{rs} - {fm}{bin_value}{rs} - {lr}{message}{rs}")
+                with open("CC_UNKNOWN.txt", "a") as file:
+                    file.write(f"{cc} [{api}] {bin_value} - {message}\n")
 
 except FileNotFoundError:
     print(f"{fr}Error: Input file '{file_path}' not found!{rs}")
